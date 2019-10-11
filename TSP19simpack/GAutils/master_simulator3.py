7096e59641a32ee20c2eb37abccb512b1ff8e7af
Anant <anantgupta@ucsb.edu>
2019-10-10 20:22:29 -0700

    Update master_simulator3.py

--- TSP19simpack/GAutils/master_simulator3.py
+++ TSP19simpack/GAutils/master_simulator3.py
@@ -203,8 +203,8 @@ def main():
         # plt.plot(rng_used, 10*np.log10(np.mean(np.sqrt(PVerror[:,:,i-3]),axis=1)#/np.mean(Ndet,axis=1) #Original
         if True:
             # Find where are non zero PVerrors
-            PVTemp = PVerror[:,:,i-3].T
-            CRBTemp = crbpv[:,:,i-3].T
+            PVTemp = PVerror[:,:,i-3]
+            CRBTemp = crbpv[:,:,i-3]
             plt.plot(rng_used, 10*np.log10([np.mean(np.sqrt(PVi[PVi>0])) for PVi in PVTemp]
                     ), color='r', label='RMSE')
             plt.plot(rng_used, 10*np.log10([np.mean(np.sqrt(CRBT[PVi>0])) for (PVi,CRBT) in zip(PVT,CRBTemp)]
